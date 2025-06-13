e22b39071ab7845527192f59d9ed2edc2ce5e86f
k-watanabe <k-watanabe@it-kensyu.com>
2025-06-13 10:48:19 +0900

    controllerフォルダに RegistControllerを作成しました。

---
+++ src/main/java/jp/co/sss/crud/controller/RegistController.java
@@ -0,0 +1,21 @@
+package jp.co.sss.crud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sss.crud.repository.EmployeeRepository;
+
+@Controller
+public class RegistController {
+	@Autowired
+	EmployeeRepository repository;
+
+	
+	
+	@RequestMapping("/input")
+	public String registInput() {
+		return "/input";
+	}
+
+}
