678409c37f446a1ebf0c804d1eab0319aca8aac8
k-watanabe <k-watanabe@it-kensyu.com>
2025-06-13 15:16:20 +0900

    a

--- src/main/java/jp/co/sss/crud/controller/RegistController.java
+++ src/main/java/jp/co/sss/crud/controller/RegistController.java
@@ -2,8 +2,12 @@ package jp.co.sss.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.crud.form.EmployeeForm;
 import jp.co.sss.crud.repository.EmployeeRepository;
 
 @Controller
@@ -13,9 +17,22 @@ public class RegistController {
 
 	
 	
-	@RequestMapping("/input")
-	public String registInput() {
-		return "/input";
+	@RequestMapping(path="/regist/input", method = RequestMethod.GET)
+	public String registInput(@ModelAttribute EmployeeForm employeeForm) {
+		return "regist/regist_input";
 	}
+	
+	@RequestMapping(path="/regist/check", method = RequestMethod.POST)
+	public String registCheck(@ModelAttribute EmployeeForm employeeForm, Model model) {
+		model.addAttribute("empPass", employeeForm.getEmpPass());
+		
+		
+		return "regist/regist_check";
+	}
+	
+	@RequestMapping(path="/regist/complete", method = RequestMethod.POST)
+	public String registComplete(@ModelAttribute EmployeeForm employeeForm) {
+		return "regist/regist_complete";
 
+    }
 }
