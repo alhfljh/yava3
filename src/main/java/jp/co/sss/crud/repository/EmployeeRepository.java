8bb0c147ed82fb436f1337246720dc3f07342589
yamazaki.katsumasa <yamazaki.katsumasa@marble-corp.co.jp>
2025-06-16 11:54:42 +0900

    EmployeeRepository.java Javadoc編集

--- src/main/java/jp/co/sss/crud/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sss/crud/repository/EmployeeRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import jp.co.sss.crud.entity.Employee;
 
 /** 社員データのリポジトリ
- * 要するに、社員データをCRUD処理する機能を持ったインターフェース*/
+ * 要するに、社員データをCRUD処理する機能を持ったインターフェース
+ */
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
+	//findBy○○で、○○を条件とした部分検索を行う
+	//EnpIdAndEmpPassが縺ｪ繧薙→縺九°繧薙→縺
 	Employee findByEmpIdAndEmpPass(int empId, String empPass);
 	List<Employee> findByEmpNameContaining(String name);
 
