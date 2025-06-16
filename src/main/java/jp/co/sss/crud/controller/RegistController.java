e030f0671016b37075d167f12b732e989530db1e
k-watanabe <k-watanabe@it-kensyu.com>
2025-06-16 12:00:02 +0900

    RegistController.java input check complete を変更しました。

--- src/main/java/jp/co/sss/crud/controller/RegistController.java
+++ src/main/java/jp/co/sss/crud/controller/RegistController.java
@@ -4,19 +4,23 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jp.co.sss.crud.bean.EmployeeBean;
+import jakarta.validation.Valid;
+import jp.co.sss.crud.entity.Department;
 import jp.co.sss.crud.entity.Employee;
 import jp.co.sss.crud.form.EmployeeForm;
+import jp.co.sss.crud.repository.DepartmentRepository;
 import jp.co.sss.crud.repository.EmployeeRepository;
 
 @Controller
 public class RegistController {
 	@Autowired
 	EmployeeRepository repository;
+	DepartmentRepository deptrepository;
 
 	
 	
@@ -25,30 +29,47 @@ public class RegistController {
 		return "regist/regist_input";
 	}
 	
+	@RequestMapping(path="/regist/inp", method = RequestMethod.POST)
+	public String registInp(@ModelAttribute EmployeeForm employeeForm) {
+		return "regist/regist_input";
+	}
+	
 	@RequestMapping(path="/regist/check", method = RequestMethod.POST)
-	public String registCheck(@ModelAttribute EmployeeForm employeeForm, Model model) {
-		model.addAttribute("empPass", employeeForm.getEmpPass());
+	public String registCheck(@Valid @ModelAttribute EmployeeForm employeeForm, BindingResult result,Model model) {
+		EmployeeForm employeeform = new EmployeeForm();
+		BeanUtils.copyProperties(employeeForm,employeeform);
+		model.addAttribute("employee",employeeform);
 		
-		Employee employee = new Employee();
-		BeanUtils.copyProperties(employeeForm,employee);
-		employee = repository.save(employee);
-		EmployeeBean employeeBean = new EmployeeBean();
-		BeanUtils.copyProperties(employee,employeeBean);
-		model.addAttribute("employee",employeeBean);
+		System.out.println(employeeform.getEmpName()+"aaaa");		
+//		System.out.println(employeeform.getDeptName()+"bbbb");		
+//		if(result.hasErrors()) {
+//			return "regist/regit_input";
+//		}
+//		if(employeeForm !=null) {
+	
 		
-		String empPass = employeeBean.getEmpPass();
-		String empName = employeeBean.getEmpName();
-		Integer gender = employeeBean.getGender();
-//		String address = registForm.getAddress();
-//		Date birthday = registForm.getBirthday();
-//		Integer authority = registForm.getAuthority();
-		
-		System.out.println(empName + gender + "見えてる？");
 		return "regist/regist_check";
+	
+//	}else {
+//		return "regist/regist_input";
+//		}
 	}
 	
 	@RequestMapping(path="/regist/complete", method = RequestMethod.POST)
 	public String registComplete(@ModelAttribute EmployeeForm employeeForm) {
+					
+		Employee employee = new Employee();
+		Department department = new Department();
+		int deptId = employeeForm.getDeptId();
+		department.setDeptId(deptId);
+
+		BeanUtils.copyProperties(employeeForm,employee);
+		employee.setDepartment(department);
+		employee = repository.save(employee);
+
+		
+		
+		
 		return "regist/regist_complete";
 
     }
