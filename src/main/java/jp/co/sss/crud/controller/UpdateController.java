f947d31229417eea3da25cf63fc6c47fb8d0090e
miyamoto.anji <miyamoto.anji@marble-corp.co.jp>
2025-06-19 15:42:04 +0900

    update 一般ユーザにも登録変更できるようになりました

--- src/main/java/jp/co/sss/crud/controller/UpdateController.java
+++ src/main/java/jp/co/sss/crud/controller/UpdateController.java
@@ -16,63 +16,87 @@ import jp.co.sss.crud.form.LoginForm;
 import jp.co.sss.crud.repository.EmployeeRepository;
 
 @Controller
- public class UpdateController {
+public class UpdateController {
 	@Autowired
 	EmployeeRepository employeeRepository;
 	HttpSession session;
-	
-	@RequestMapping(path="/update/manage")
-	public String updateManage(@ModelAttribute LoginForm loginForm,HttpServletRequest request,Model model) {
 
-			return "/update/update_manage";
-		
-	}
-	
-	@RequestMapping(path="/update/input",method= RequestMethod.POST)
-	public String upDate(@ModelAttribute EmployeeForm employeeForm,Model model) {
-		 
-			Integer empId = employeeForm.getEmpId();
-			System.out.println(empId+"ID");
-			Employee employee = employeeRepository.findByEmpId(empId);
-			
-			if (employee != null) {
-				
-				employeeForm.setEmpId(employee.getEmpId());
-				employeeForm.setEmpName(employee.getEmpName());
-				employeeForm.setGender(employee.getGender());
-				employeeForm.setAddress(employee.getAddress());
-				employeeForm.setBirthday(employee.getBirthday());
-				employeeForm.setAuthority(employee.getAuthority());
-				System.out.println("にこにこ");
-				return "update/update_input";
+	@RequestMapping(path = "/update/manage1")
+	public String updateUser(@ModelAttribute EmployeeForm employeeForm,HttpSession session, Model model) {
+		Integer empId = (Integer)session.getAttribute("userId");
+//		EmployeeForm emp = (EmployeeForm)session.getAttribute("user");
+//		System.err.println(emp);
+		Employee employee = employeeRepository.findByEmpId(empId);
+		employeeForm.setEmpId(employee.getEmpId());
+		employeeForm.setEmpName(employee.getEmpName());
+		employeeForm.setGender(employee.getGender());
+		employeeForm.setAddress(employee.getAddress());
+		employeeForm.setBirthday(employee.getBirthday());
+		employeeForm.setAuthority(employee.getAuthority());
 
-				
-			} else {
-				model.addAttribute("errMessage", "社員IDが存在しません。");
-				return  "list/list";
-			}
-		
-		
+		return "/update/update_user";
 	}
+
+	@RequestMapping(path = "/update/manage2")
+	public String updateManage(@ModelAttribute LoginForm loginForm, HttpServletRequest request, Model model) {
+		//		コントローラ側での管理者権限
+		//		HttpSession session = request.getSession();
+		//		session.getAttribute("manage");
+		//		Integer manage = (Integer) session.getAttribute("manage");
+		//		if ( manage == 2) {
+		//			
+		//			return "/update/update_manage";
+		//		} else {
+		//			model.addAttribute("errMessage", "権限がありません");
+		//			return "list/list";
+		//		}
+		model.addAttribute("emp", employeeRepository.findAllByOrderByEmpIdAsc());
+		return "/update/update_manage";
+
+	}
+
+	@RequestMapping(path = "/update/input", method = RequestMethod.POST)
+	public String upDate(@ModelAttribute EmployeeForm employeeForm, Model model) {
+
+		Integer empId = employeeForm.getEmpId();
+		System.out.println(empId + "ID");
+		Employee employee = employeeRepository.findByEmpId(empId);
+
+		if (employee != null) {
+
+			employeeForm.setEmpId(employee.getEmpId());
+			employeeForm.setEmpName(employee.getEmpName());
+			employeeForm.setGender(employee.getGender());
+			employeeForm.setAddress(employee.getAddress());
+			employeeForm.setBirthday(employee.getBirthday());
+			employeeForm.setAuthority(employee.getAuthority());
+			return "update/update_input";
+
+		} else {
+			model.addAttribute("errMessage", "社員IDが存在しません。");
+			return "list/list";
+		}
+
+	}
+
 	@RequestMapping(path = "/update/back", method = RequestMethod.POST)
 	public String registInp(@ModelAttribute EmployeeForm employeeForm) {
 		return "update/update_input";
 	}
-	
-	@RequestMapping(path="/update/checked", method=RequestMethod.POST)
-	public String updateCheak(@ModelAttribute EmployeeForm employeeForm,Model model) {
-		    EmployeeForm employeeform = new EmployeeForm();
-			BeanUtils.copyProperties(employeeForm, employeeform);
-			model.addAttribute("update_employee", employeeform);
-			return "update/update_check";
+
+	@RequestMapping(path = "/update/checked", method = RequestMethod.POST)
+	public String updateCheak(@ModelAttribute EmployeeForm employeeForm, Model model) {
+		EmployeeForm employeeform = new EmployeeForm();
+		BeanUtils.copyProperties(employeeForm, employeeform);
+		model.addAttribute("update_employee", employeeform);
+		return "update/update_check";
 	}
-	@RequestMapping(path="/update/complete",method = RequestMethod.POST)
-	public String updateConpelete(@ModelAttribute EmployeeForm employeeForm,Model model) {
-		System.out.println(employeeForm.getEmpId()+"出てますか");
+
+	@RequestMapping(path = "/update/complete", method = RequestMethod.POST)
+	public String updateConpelete(@ModelAttribute EmployeeForm employeeForm, Model model) {
 		Integer empId = employeeForm.getEmpId();
-		System.out.println(empId+"社員");
 		Employee employee = employeeRepository.getReferenceById(empId);
-		BeanUtils.copyProperties(employeeForm, employee,"empId");
+		BeanUtils.copyProperties(employeeForm, employee, "empId");
 		employee = employeeRepository.save(employee);
 		return "/update/update_complete";
 	}
