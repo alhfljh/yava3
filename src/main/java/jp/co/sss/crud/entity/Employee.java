d854ecfa770fd368539c7346801f249a123acf12
yamashita <yamashita.keishi@marble-corp.co.jp>
2025-06-23 13:31:57 +0900

    u

--- src/main/java/jp/co/sss/crud/entity/Employee.java
+++ src/main/java/jp/co/sss/crud/entity/Employee.java
@@ -19,166 +19,182 @@ import jakarta.persistence.Table;
 @Table(name = "employee")
 public class Employee {
 	/**
-	 * 社員IDの列を作成
+	 * 社員ID
 	 * 主キー社員ID
 	 * シーケンスによってIDが自動採番される
 	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_emp_gen")
-	@SequenceGenerator(name="seq_emp_gen", sequenceName ="seq_emp",allocationSize = 1)
+	@SequenceGenerator(name = "seq_emp_gen", sequenceName = "seq_emp", allocationSize = 1)
 	private Integer empId;
-	
-	/**社員のパスワード（文字列型）の列を作成*/
+
+	/**社員のパスワード（文字列型）*/
 	@Column
 	private String empPass;
 
-	/**社員名の（文字列型）の列を作成*/
+	/**社員名（文字列型）*/
 	@Column
 	private String empName;
 
-	/**社員の性別（数値型）の列を作成*/
+	/**社員の性別（数値型）*/
 	@Column
 	private Integer gender;
 
-	/**社員の住所（文字列型）の列を作成*/
+	/**社員の住所（文字列型）*/
 	@Column
 	private String address;
 
-	/**社員の生年月日（yyyy/MM/dd）の列を作成*/
+	/**社員の生年月日（yyyy/MM/dd）*/
 	@Column
 	private Date birthday;
-	
-	/**社員の権限（数値型）の列を作成*/
+
+	/**社員の権限（数値型）*/
 	@Column
 	private Integer authority;
-	
+
+	/** */
 	@ManyToOne
-	@JoinColumn(name="dept_id", referencedColumnName = "deptId")
+	@JoinColumn(name = "dept_id", referencedColumnName = "deptId")
 	private Department department;
-	
-	 /**
+
+	/**削除フラグ*/
+	@Column
+	private Integer deleteFlag;
+
+	/**
 	 * 社員IDの取得
 	 * @return　社員ID
 	 */
 	public Integer getEmpId() {
-	        return empId;
-	    }
+		return empId;
+	}
 
-	    /**
-	     *社員IDのセット 
-	     * @param empId
-	     */
-	    public void setEmpId(Integer empId) {
-	        this.empId = empId;
-	    }
+	/**
+	 *社員IDのセット 
+	 * @param empId
+	 */
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
 
-	    /**
-	     * 社員パスワードの取得
-	     * @return 社員パスワード
-	     */
-	    public String getEmpPass() {
-	        return empPass;
-	    }
+	/**
+	 * 社員パスワードの取得
+	 * @return 社員パスワード
+	 */
+	public String getEmpPass() {
+		return empPass;
+	}
 
-	    /**
-	     * 社員パスワードのセット
-	     * @param empPass
-	     */
-	    public void setEmpPass(String empPass) {
-	        this.empPass = empPass;
-	    }
+	/**
+	 * 社員パスワードのセット
+	 * @param empPass
+	 */
+	public void setEmpPass(String empPass) {
+		this.empPass = empPass;
+	}
 
-	    /**
-	     *社員名の取得
-	     * @return 社員名
-	     */
-	    public String getEmpName() {
-	        return empName;
-	    }
+	/**
+	 *社員名の取得
+	 * @return 社員名
+	 */
+	public String getEmpName() {
+		return empName;
+	}
 
-	    /**
-	     * 社員名のセット
-	     * @param empName
-	     */
-	    public void setEmpName(String empName) {
-	        this.empName = empName;
-	    }
+	/**
+	 * 社員名のセット
+	 * @param empName
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
 
-	    /**
-	     * 社員の性別の取得
-	     * @return 社員の性別
-	     */
-	    public Integer getGender() {
-	        return gender;
-	    }
+	/**
+	 * 社員の性別の取得
+	 * @return 社員の性別
+	 */
+	public Integer getGender() {
+		return gender;
+	}
 
-	    /**
-	     * 社員の性別のセット
-	     * @param gender
-	     */
-	    public void setGender(Integer gender) {
-	        this.gender = gender;
-	    }
+	/**
+	 * 社員の性別のセット
+	 * @param gender
+	 */
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
 
-	    
-	    /**
-	     * 社員の住所の取得
-	     * @return 社員の住所
-	     */
-	    public String getAddress() {
-	        return address;
-	    }
-	    /**
-	     * 社員の住所のセット
-	     * @param address
-	     */
-	    public void setAddress(String address) {
-	        this.address = address;
-	    }
+	/**
+	 * 社員の住所の取得
+	 * @return 社員の住所
+	 */
+	public String getAddress() {
+		return address;
+	}
 
-	    /**
-	     * 社員の生年月日の取得
-	     * @return 社員の生年月日
-	     */
-	    public Date getBirthday() {
-	        return birthday;
-	    }
-	    /**
-	     * 社員の生年月日のセット
-	     * @param birthday
-	     */
-	    public void setBirthday(Date birthday) {
-	        this.birthday = birthday;
-	    }
+	/**
+	 * 社員の住所のセット
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-	    /**
-	     * 社員の権限の取得
-	     * @return 社員の権限
-	     */
-	    public Integer getAuthority() {
-	        return authority;
-	    }
-	    /**
-	     * 社員の権限のセット
-	     * @param authority
-	     */
-	    public void setAuthority(Integer authority) {
-	        this.authority = authority;
-	    }
+	/**
+	 * 社員の生年月日の取得
+	 * @return 社員の生年月日
+	 */
+	public Date getBirthday() {
+		return birthday;
+	}
 
-		/**
-		 * 社員の部署名の取得
-		 * @return 社員の部署名
-		 */
-		public Department getDepartment() {
-			return department;
-		}
-		 /**
-	     * 社員の部署名のセット
-	     * @param department
-	     */
-		public void setDepartment(Department department) {
-			this.department = department;
-		}
+	/**
+	 * 社員の生年月日のセット
+	 * @param birthday
+	 */
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	/**
+	 * 社員の権限の取得
+	 * @return 社員の権限
+	 */
+	public Integer getAuthority() {
+		return authority;
+	}
+
+	/**
+	 * 社員の権限のセット
+	 * @param authority
+	 */
+	public void setAuthority(Integer authority) {
+		this.authority = authority;
+	}
+
+	/**
+	 * 社員の部署名の取得
+	 * @return 社員の部署名
+	 */
+	public Department getDepartment() {
+		return department;
+	}
+
+	/**
+	* 社員の部署名のセット
+	* @param department
+	*/
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Integer getDeleteFlag() {
+		return deleteFlag;
+	}
+
+	public void setDeleteFlag(Integer deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
 
 }
--- src/main/java/jp/co/sss/crud/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sss/crud/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package jp.co.sss.crud.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import jp.co.sss.crud.entity.Employee;
 
@@ -26,6 +28,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	Employee findByEmpIdAndEmpPass(int empId, String empPass);
 
 	/** 
+	 * 検索
 	 * 社員エンティティ（List<Employee>）から、
 	 *検索した文字列（EmpName）が含まれている（Containing）レコードを検索
 	 *→社員名検索にて、入力した文字列が社員一覧の表にあるかどうかを確認するためのもの
@@ -33,11 +36,17 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	 * @param name
 	 * @return 社員一覧表示画面（list.html）
 	 */
-	List<Employee> findByEmpNameContaining(String name);
+	@Query("SELECT e FROM Employee e WHERE e.empName LIKE %:name% AND e.deleteFlag = :deleteFlag ORDER BY e.empId ASC")
+	List<Employee> findByEmpNameContaining(@Param("name") String name);
 
 	Employee findByEmpId(Integer empId);
 
+	@Query("SELECT e FROM Employee e WHERE e.deleteFlag = 0 ORDER BY e.empId ASC")
 	List<Employee> findAllByOrderByEmpIdAsc();
+	
+//	@Query("SELECT e FROM Employee e WHERE e.deleteFlag = 0  ORDER BY e.empId ASC")
+//	List<Employee> findByDeleteFlagQuery();
+
 
 	
 
