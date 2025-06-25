b69279b905f94c544ab2043126bf32c25cb1beef
yamazaki.katsumasa <yamazaki.katsumasa@marble-corp.co.jp>
2025-06-25 11:53:07 +0900

    DeleteController.java 一般権限の時も退職者含む一覧画面が正常に表示されるように、URLを変更

--- src/main/java/jp/co/sss/crud/controller/DeleteController.java
+++ src/main/java/jp/co/sss/crud/controller/DeleteController.java
@@ -3,14 +3,12 @@ package jp.co.sss.crud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import jp.co.sss.crud.entity.Employee;
 import jp.co.sss.crud.form.EmployeeForm;
 import jp.co.sss.crud.repository.DepartmentRepository;
@@ -62,7 +60,7 @@ public class DeleteController {
 		return "delete/delete_complete";
 	}
 
-	@RequestMapping(path = "/list/delete")
+	@RequestMapping(path = "/list/delFlg")
 	public String listDelete(Model model) {
 		model.addAttribute("emp", employeeRepository.findDeleteByOrderByEmpIdAsc());
 		model.addAttribute("dept", departmentRepository.findAllByOrderByDeptIdAsc());
